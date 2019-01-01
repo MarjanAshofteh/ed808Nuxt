3d1e568cb689d11b3c21fb71c1e2e15cd9bb635f
MarjanAshofteh <marjan.cs90@gmail.com>
2019-01-01 18:45:37 +0330

    convert store code

--- store/index.js
+++ store/index.js
@@ -2,48 +2,44 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-// Create new Store
-const store = new Vuex.Store({
-  //strict: process.env.NODE_ENV !== 'production',
-  state:() => ({
-    uid:0,
-    roles:['administrator'],
-    //allContents page
-    filtersLoading: true,
-    selected : {
-      type: [],
-      category: [],
-      topic: [],
-    },
-    queryNames : {
-      t2258: 'product_kind',
-      t2259: 'field',
-      t2260: 'publication_name',
-      t2261: 'exam',
-      t2262: 'education_kind',
-      t2264: 'academic_orientation',
-      t2265: 'special',
-      t2458: 'producer',
-    },
-    filters : {}
-  }),
-  mutations:{
-    LOGIN(state,uid){
-      state.uid = uid
-    },
-    LOGOUT(state){
-      state.uid = 0
-    },
-    SET_FILTERS(state, filters){
-      state.filters = filters
-    },
+
+export const state = () => ({
+  uid:0,
+  roles:['administrator'],
+  //allContents page
+  filtersLoading: true,
+  selected : {
+    type: [],
+    category: [],
+    topic: [],
   },
-  getters:{
-    getUid: state => (state.uid > 0)? state.uid : false
+  queryNames : {
+    t2258: 'product_kind',
+    t2259: 'field',
+    t2260: 'publication_name',
+    t2261: 'exam',
+    t2262: 'education_kind',
+    t2264: 'academic_orientation',
+    t2265: 'special',
+    t2458: 'producer',
   },
-  actions:{
-  }
+  filters : {}
 })
 
-export default store
+export const mutations = {
+  LOGIN(state,uid){
+    state.uid = uid
+  },
+  LOGOUT(state){
+    state.uid = 0
+  },
+  SET_FILTERS(state, filters){
+    state.filters = filters
+  },
+}
+
+export const getters = {
+  getUid: state => (state.uid > 0)? state.uid : false
+}
+
 
