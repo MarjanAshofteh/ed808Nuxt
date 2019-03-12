6d987b7dd9efccd8019e93dec8212221051d603d
MarjanAshofteh <marjan.cs90@gmail.com>
2019-03-12 18:53:59 +0330

    add tabs to activity tab

--- layouts/userpanel.vue
+++ layouts/userpanel.vue
@@ -845,12 +845,7 @@
 </style>
 <style>
   .md-tabs.md-theme-default .md-tabs-navigation{
-    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
     display: flex;
     justify-content: center;
-    padding-right: 21%;
-  }
-  .md-tabs.md-theme-default .md-tabs-content{
-    height:initial !important;
   }
 </style>
--- pages/user/_uid/activity.vue
+++ pages/user/_uid/activity.vue
@@ -1,15 +1,22 @@
 <template>
-  <!-- liked comment + author comment + bookmarked node + clap node -->
-  <div>
-    <div class="spinner-loading" v-if="spinner_loading">
-      <md-progress-spinner :md-diameter="100" :md-stroke="5" md-mode="indeterminate"></md-progress-spinner>
-    </div>
-    <div class="bookmarked_node md-layout" style="position: relative;margin: 0 -1%;">
-        <NodeTeaser v-for="post in bookmarked_node.contents" :title="post.title" :pic="post.picture" :date="post.created" :nid="post.nid" />
-    </div>
-    <div class="clap_node md-layout" style="position: relative;margin: 0 -1%;">
-      <NodeTeaser v-for="post in clap_node.contents" :title="post.title" :pic="post.picture" :date="post.created" :nid="post.nid" />
-    </div>
+  <!-- bookmarked node + clap node + liked comment + author comment +  -->
+  <div id="activity">
+
+    <md-tabs>
+      <md-tab id="tab-home" md-label="Bookmark" md-icon="turned_in">
+        <div class="bookmarked_node md-layout" style="position: relative;margin: 0 -1%;">
+          <NodeTeaser v-for="post in bookmarked_node.contents" :title="post.title" :pic="post.picture" :date="post.created" :nid="post.nid" />
+        </div>
+      </md-tab>
+      <md-tab id="tab-pages" md-label="clap" md-icon="stars">
+        <div class="clap_node md-layout" style="position: relative;margin: 0 -1%;">
+          <NodeTeaser v-for="post in clap_node.contents" :title="post.title" :pic="post.picture" :date="post.created" :nid="post.nid" />
+        </div>
+      </md-tab>
+      <md-tab id="tab-favorites" md-label="comment" md-icon="mode_comment"></md-tab>
+      <md-tab id="tab-posts" md-label="Favorite" md-icon="favorite"></md-tab>
+    </md-tabs>
+
   </div>
 </template>
 
@@ -33,6 +40,7 @@
     },
     mounted(){
       this.bookmarkedNode()
+      this.clappedNode()
     },
     methods:{
       bookmarkedNode(){
@@ -52,7 +60,7 @@
             this.errors = e.response.data
           })
       },
-      bookmarkedNode(){
+      clappedNode(){
         axios.defaults.crossDomain = true
         axios.defaults.withCredentials  = true
         axios.get('https://ed808.com:92/latin/user/'+ this.uid + '/favorite_content?parameter[page]='+ this.page +'&parameter[limits]=' + 10 ,
@@ -73,7 +81,32 @@
   }
 </script>
 
-<style lang="scss" scoped>
-  .bookmarked_node{
+<style lang="scss">
+  #activity{
+    .md-tabs.md-theme-default .md-tabs-navigation .md-button {
+      width: 100px;
+      height: 104px;
+      border-radius: 5px;
+      margin: 5px;
+      &.md-active {
+        background-color: #4caf50 !important;
+        -webkit-box-shadow: 0 12px 20px -10px rgba(76, 175, 80, .28), 0 4px 20px 0 rgba(0, 0, 0, .12), 0 7px 8px -5px rgba(76, 175, 80, .2);
+        box-shadow: 0 12px 20px -10px rgba(76, 175, 80, .28), 0 4px 20px 0 rgba(0, 0, 0, .12), 0 7px 8px -5px rgba(76, 175, 80, .2);
+
+        * {
+          color: white !important;
+        }
+      }
+    }
+    .md-tabs.md-theme-default .md-tabs-navigation .md-button:not(.md-active):hover {
+      background-color: hsla(0, 0%, 78%, .2) !important;
+    }
+    .md-tabs-navigation .md-icon-label .md-tab-icon+.md-tab-label {
+      margin-top: 15px;
+    }
+    .md-tabs-indicator{
+      display: none;
+    }
   }
+
 </style>
